7738b41d40c6422dbbb146badc20dd16ccb71105
Tina Azad <mktazad@gmail.com>
2019-10-30 12:46:12 -0400

    style added

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var methodOverride = require ('method-override')
 var exphbs = require ('express-handlebars');
 
 var app = express();
-app.use(express.static('_dirname' +'/public'));
+app.use(express.static(__dirname + '/public'));
 //
 
 app.use (bodyParser.urlencoded({
